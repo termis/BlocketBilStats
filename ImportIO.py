2dc8e403d3d0c0992eb7cd6635f86d2900f02660
geckah <geckah@gmail.com>
2016-09-27 22:34:18 +0200

    home update

--- ImportIO.py
+++ ImportIO.py
@@ -1,22 +1,31 @@
 try: import simplejson as json
 except ImportError: import json
 from pprint import pprint
+import os
 import re
 
-with open("C:\\Users\\SERAPALISG\\PycharmProjects\\BlocketBilStats\\json\\20160612.json", encoding='utf8') as data_file:
+IOimportPathJson = "~\\PycharmProjects\\BlocketBilStats\\json\\20160612.json"
+#with open("C:\\Users\\SERAPALISG\\PycharmProjects\\BlocketBilStats\\json\\20160612.json", encoding='utf8') as data_file:
+with open(os.path.expanduser(IOimportPathJson), encoding='utf8') as data_file:
     data = json.load(data_file)
     data = data['result']['extractorData']['data'] # data holds list
     data = data[0]['group'][0:] # list with all needed info
     # data_keys = data['result']['extractorData'].keys()
 
+
 #pprint(data)
 for x in data:
     pprint(x['Item link'])
     pprint(x['Listprice number'])
-    # for z in x['Listprice number']:
-    #     z = z['text']
-    #     z = re.sub("[^0-9]", "", z)
-    #     pprint(int(z))
+
+    for z in x['Item link']:
+        title = z['text']
+        pprint(title)
+
+    for z in x['Listprice number']:
+        price = z['text']
+        price = re.sub(r"[^0-9]", "", price)
+        pprint(int(price))
 
 
 #x = data
